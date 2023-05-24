9e9d929c60d305be8c7461570977a942fbd00044
Ваня Кукушкин <112860454+Noxi0072@users.noreply.github.com>
2023-05-24 19:26:21 +0300

    Update Calculate.java

--- Java практические задания/задание 50 /Calculate.java
+++ Java практические задания/задание 50 /Calculate.java
@@ -3,12 +3,9 @@ package ru.Vsuet.mtx;
 import java.util.Stack;
 
 public class Calculate {
-    // Метод для проверки, является ли символ оператором
     private boolean isOperator(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '|' || c == '%';
     }
-
-    // Метод для выполнения операции над двумя числами
     private double executeOperation(double num1, double num2, char operator) {
         switch (operator) {
             case '+':
@@ -27,21 +24,15 @@ public class Calculate {
                 throw new IllegalArgumentException("Неверный оператор: " + operator);
         }
     }
-
-    // Метод для вычисления значения математического выражения
     public double evaluateExpression(String expression) {
         Stack<Double> numbers = new Stack<>();
         Stack<Character> operators = new Stack<>();
 
         for (int i = 0; i < expression.length(); i++) {
             char currentChar = expression.charAt(i);
-
             if (Character.isDigit(currentChar)) {
-                // Если текущий символ - цифра, извлекаем число из строки и добавляем в стек чисел
                 StringBuilder numBuilder = new StringBuilder();
                 numBuilder.append(currentChar);
-
-                // Проверяем следующие символы, чтобы извлечь полное число
                 while (i + 1 < expression.length() && Character.isDigit(expression.charAt(i + 1))) {
                     numBuilder.append(expression.charAt(i + 1));
                     i++;
@@ -49,11 +40,8 @@ public class Calculate {
 
                 double number = Double.parseDouble(numBuilder.toString());
                 numbers.push(number);
-            } else if (currentChar == '(') {
-                // Если текущий символ - открывающая скобка, добавляем ее в стек операторов
                 operators.push(currentChar);
             } else if (currentChar == ')') {
-                // Если текущий символ - закрывающая скобка, выполняем операции внутри скобок
                 while (!operators.isEmpty() && operators.peek() != '(') {
                     char operator = operators.pop();
                     double num2 = numbers.pop();
@@ -61,13 +49,10 @@ public class Calculate {
                     double result = executeOperation(num1, num2, operator);
                     numbers.push(result);
                 }
-
-                // Удаляем открывающую скобку из стека операторов
                 if (!operators.isEmpty() && operators.peek() == '(') {
                     operators.pop();
                 }
             } else if (isOperator(currentChar)) {
-                // Если текущий символ - оператор, выполняем операции в зависимости от приоритета
                 while (!operators.isEmpty() && getOperatorPriority(operators.peek()) >= getOperatorPriority(currentChar)) {
                     char operator = operators.pop();
                     double num2 = numbers.pop();
@@ -75,13 +60,9 @@ public class Calculate {
                     double result = executeOperation(num1, num2, operator);
                     numbers.push(result);
                 }
-
-                // Добавляем текущий оператор в стек операторов
                 operators.push(currentChar);
             }
         }
-
-        // Выполняем оставшиеся операции
         while (!operators.isEmpty()) {
             char operator = operators.pop();
             double num2 = numbers.pop();
@@ -89,12 +70,8 @@ public class Calculate {
             double result = executeOperation(num1, num2, operator);
             numbers.push(result);
         }
-
-        // Возвращаем конечный результат
         return numbers.pop();
     }
-
-    // Метод для определения приоритета оператора
     private int getOperatorPriority(char operator) {
         switch (operator) {
             case '+':
